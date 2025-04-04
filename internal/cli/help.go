2f97dc6b1a578edd4a3459b9645873b2525aa5b1
Yousef Akbar <yhakbar99@gmail.com>
2025-04-05 01:43:18 +0300

    Add help message for browse command

--- internal/cli/help.go
+++ internal/cli/help.go
@@ -17,6 +17,7 @@ func runCommandHelp(cfg *config.ConfigManager, args []string) error {
 	fmt.Println("Usage: o <command> [options]")
 	fmt.Println("")
 	fmt.Println("Available Commands (Shorthand, Command):")
+	fmt.Printf("  b, browse\t\t-- Browse vault files with `gum`\n")
 	fmt.Printf("  h, help\t\t-- Prints this help message\n")
 	fmt.Printf("  n, new\t\t-- Creates a new note\n")
 	fmt.Printf("  s, search\t\t-- Searches and opens note in EDITOR\n")
